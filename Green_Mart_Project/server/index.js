2b4f531ddc8d5ee55de8a5a3a8896f08b35e1167
tushar1805mandge <125894873+tushar1805mandge@users.noreply.github.com>
2025-06-13 23:30:25 +0530

    Added routers and utils, updated server/index.js

--- Green_Mart_Project/server/index.js
+++ Green_Mart_Project/server/index.js
@@ -1,1 +1,20 @@
-ECHO is on.
+const express = require("express")
+const app = express()
+const productRouter = require("./routers/product")
+// const reviewRouter = require("./routers/review")
+// const userRouter = require("./routers/users")
+//const {jwtAuth} = require("./utils/jwtauth")
+
+const cors  =require("cors")
+app.use(cors())
+
+app.use(express.json());
+//app.use(jwtAuth);
+app.use("/product", productRouter);
+// app.use("/review",reviewRouter);
+// app.use("/users",userRouter);
+
+const port = 4000;
+app.listen(port, "0.0.0.0", () => {
+	console.log("server ready at port", port);
+});
