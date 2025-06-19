0b70c9cd7489036151b71d4d6d44609184fe0c26
Saurabh Madrap <86643002+saurabhmadrap07@users.noreply.github.com>
2025-06-19 16:55:35 +0530

    React Native Application setup done lets build it

---
+++ Green_Mart_Project/Frontend_ReactNative/app/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Green_Mart_Application_Is_Ready_To_Build</Text>
+    </View>
+  );
+}
